a13a96cc47c91c0f68c03d1647704e02afe65e69
clraifo <41687507+clraifo@users.noreply.github.com>
2023-09-01 14:08:43 -0400

    Create charToMorse.c

---
+++ charToMorse.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+// Function to convert a character to its Morse code representation
+const char* charToMorse(char c) {
+    switch (c) {
+        case 'A': return ".-";
+        case 'B': return "-...";
+        case 'C': return "-.-.";
+        case 'D': return "-..";
+        case 'E': return ".";
+        case 'F': return "..-.";
+        case 'G': return "--.";
+        case 'H': return "....";
+        case 'I': return "..";
+        case 'J': return ".---";
+        case 'K': return "-.-";
+        case 'L': return ".-..";
+        case 'M': return "--";
+        case 'N': return "-.";
+        case 'O': return "---";
+        case 'P': return ".--.";
+        case 'Q': return "--.-";
+        case 'R': return ".-.";
+        case 'S': return "...";
+        case 'T': return "-";
+        case 'U': return "..-";
+        case 'V': return "...-";
+        case 'W': return ".--";
+        case 'X': return "-..-";
+        case 'Y': return "-.--";
+        case 'Z': return "--..";
+        case '1': return ".----";
+        case '2': return "..---";
+        case '3': return "...--";
+        case '4': return "....-";
+        case '5': return ".....";
+        case '6': return "-....";
+        case '7': return "--...";
+        case '8': return "---..";
+        case '9': return "----.";
+        case '0': return "-----";
+        case ',': return "--..--";
+        case '.': return ".-.-.-";
+        case '?': return "..--..";
+        case '!': return "-.-.--";
+        case ')': return "-.--.-";
+        default: return "";
+    }
+}
+
+// Test the function
+int main() {
+    char testChars[] = "AB1,.";
+    for (int i = 0; i < strlen(testChars); i++) {
+        printf("Character: %c, Morse Code: %s\n", testChars[i], charToMorse(testChars[i]));
+    }
+    return 0;
+}
